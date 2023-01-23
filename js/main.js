069c7e9f8eb36ccc9937d358fb4755facdad1770
Adam Bishop <adam.michael.bishop@gmail.com>
2023-01-23 12:27:38 -0600

    Features -Defined map styles and map objects -Added a map to the page that takes up the entire page and displays when it is finished loading

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
 "use strict";
 
-import {MAPBOX_API_KEY, OPENWEATHER_API_KEY} from "./keys";
+import {MAPBOX_API_KEY, OPENWEATHER_API_KEY} from "./keys.js";
+
+mapboxgl.accessToken = MAPBOX_API_KEY;
+const mapStyles = {
+    streets: 'mapbox://styles/mapbox/streets-v12',
+    satellite: 'mapbox://styles/mapbox/satellite-v9',
+    light: 'mapbox://styles/mapbox/light-v11',
+    dark: 'mapbox://styles/mapbox/dark-v11'
+}
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: mapStyles.streets,
+    zoom: 8,
+    center: [-98.4916, 29.4252]
+});
+
+function createMarker(coords) {
+    return new mapboxgl.Marker()
+        .setLngLat(coords)
+        .addTo(map);
+}
+
+function search() {
+
+}
+
+map.on('load', function () {
+    $('#map').height($(window).height());
+    map.resize()
+        .addControl(
+            new mapboxgl.GeolocateControl({
+                positionOptions: {
+                    enableHighAccuracy: true
+                },
+                // When active the map will receive updates to the device's location as it changes.
+                trackUserLocation: true,
+                // Draw an arrow next to the location dot to indicate which direction the device is heading.
+                showUserHeading: false
+            })
+        );
+});
